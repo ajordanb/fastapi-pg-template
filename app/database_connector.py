0da415222446614b2276ce7d76fef1549953a805
Alejandro Jordan <ajordanbojanic@gmail.com>
2022-11-22 08:46:09 -0600

    added cloud sql postgresql connector config

---
+++ app/database_connector.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from google.cloud.sql.connector import Connector, IPTypes
+from .config import settings
+
+# Python Connector database connection function
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            settings.database_hostname, # Cloud SQL Instance Connection Name
+            "pg8000",
+            user=settings.database_username,
+            password=settings.database_password,
+            db=settings.database_name,
+            ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+        )
+    return conn
+
+SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL , creator=getconn
+)
+
+# create SQLAlchemy ORM session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
