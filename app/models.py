9f3d33e04ceaf55549874933d1f7c5707eb68f7f
Alejandro Jordan <ajordanbojanic@gmail.com>
2023-02-22 10:15:59 -0600

    adding user management routes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ARRAY
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
@@ -14,3 +14,14 @@ class User(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
+    
+class Scopes(Base):
+    __tablename__= "scope"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name= Column(String, nullable=False, unique=True)
+      
+class UserScopes(Base):
+    __tablename__="user_scope"
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(String, ForeignKey("user.id"))
+    scope_id = Column(String, ForeignKey("scope.id"))
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,10 +1,10 @@
-from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi import Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, utils, oauth2
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import List
 
-router = APIRouter(prefix="/user", tags=["Users"])
+router = APIRouter(prefix="/user", tags=["Manage Users"])
 
 
 @router.get(
@@ -12,13 +12,18 @@ router = APIRouter(prefix="/user", tags=["Users"])
     status_code=status.HTTP_200_OK,
     response_model=List[schemas.UserOut],
 )
-async def get_users(db: Session = Depends(get_db)):
+async def get_users(db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+                    ):
     users = db.query(models.User).all()
     return users
 
 
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
-async def get_user(id: int, db: Session = Depends(get_db)):
+@router.get("/get", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
+async def get_user(id: int, db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+                   
+                   ):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(
@@ -28,13 +33,12 @@ async def get_user(id: int, db: Session = Depends(get_db)):
     return user
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+@router.post("/create", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 async def create_user(
     user: schemas.UserIn,
     db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.verify_admin),
+    admin: str = Depends(oauth2.verify_admin),
 ):
-    # print(current_user)
     hashed_password = utils.hash_password(user.password)
     user.password = hashed_password
     new_user = models.User(**user.dict())
@@ -42,3 +46,19 @@ async def create_user(
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.delete("/delete", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(
+    id: int,
+    db: Session = Depends(get_db),
+    admin: str = Depends(oauth2.verify_admin),
+):
+    user_query = db.query(models.User).filter(models.User.id == id)
+    user = user_query.first()
+    if user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user with id: {id} does not exist")
+
+    user_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
