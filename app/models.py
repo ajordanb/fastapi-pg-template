b545a9c0dca4ebaae27fe76ff2305d306cac10a5
Alejandro Jordan <ajordanbojanic@gmail.com>
2023-02-22 10:24:15 -0600

    removing unnecesary models

--- app/models.py
+++ app/models.py
@@ -15,13 +15,3 @@ class User(Base):
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
     
-class Scopes(Base):
-    __tablename__= "scope"
-    id = Column(Integer, primary_key=True, nullable=False)
-    name= Column(String, nullable=False, unique=True)
-      
-class UserScopes(Base):
-    __tablename__="user_scope"
-    id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(String, ForeignKey("user.id"))
-    scope_id = Column(String, ForeignKey("scope.id"))
